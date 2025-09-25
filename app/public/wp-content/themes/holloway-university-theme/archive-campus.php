bae8b26ceaa25b92ed5c2ef1de27fe4933e6a6ad
Alex Holloway <hollowayalexh@gmail.com>
2025-09-25 08:29:12 -0500

    fix file name, add campus post-type, create package.json for wordpress/javascript package

---
+++ app/public/wp-content/themes/holloway-university-theme/archive-campus.php
@@ -0,0 +1,24 @@
+<?php 
+  get_header(); 
+  pageBanner(array(
+    'title' => 'Our Campuses',
+    'subtitle' => 'Different Settings, Same Commitment to You.'
+  ))
+  ?>
+      <div class="container container--narrow page-section">
+        <ul class="link-list min-list">
+            <?php 
+            while (have_posts()) {
+                the_post(); ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </li>
+            <?php }
+            echo paginate_links();
+            ?>
+        </ul>
+
+  <?php get_footer();
+?>
---
+++ app/public/wp-content/themes/holloway-university-theme/archive-program.php
@@ -0,0 +1,24 @@
+<?php 
+  get_header(); 
+  pageBanner(array(
+    'title' => 'Programs',
+    'subtitle' => 'Check out all we have to offer.'
+  ))
+  ?>
+      <div class="container container--narrow page-section">
+        <ul class="link-list min-list">
+            <?php 
+            while (have_posts()) {
+                the_post(); ?>
+                <li>
+                    <a href="<?php the_permalink(); ?>">
+                        <?php the_title(); ?>
+                    </a>
+                </li>
+            <?php }
+            echo paginate_links();
+            ?>
+        </ul>
+
+  <?php get_footer();
+?>
