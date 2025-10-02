7fd95fc1bb1b53c7babf8a925b4e60c3d2a9d3f9
Alex Holloway <hollowayalexh@gmail.com>
2025-10-02 16:14:09 -0500

    update search functionality to include related fields

--- app/public/wp-content/themes/holloway-university-theme/inc/search-route.php
+++ app/public/wp-content/themes/holloway-university-theme/inc/search-route.php
@@ -49,6 +49,7 @@ function universitySearchResults($data) {
                 'title' => get_the_title(),
                 'link' => get_the_permalink(),
                 'authorName' => get_the_author(),
+                'id' => get_the_ID(),
             ));
         }
         if (get_post_type() == 'campus') {
@@ -60,15 +61,16 @@ function universitySearchResults($data) {
             ));
         }
 
-        $eventDate = new DateTime(get_field('event_date'));
-        $description = null;
-        if (has_excerpt()) {
-            $description = get_the_excerpt();
-        } else {
-            $description = wp_trim_words(get_the_content(), 18);
-        }
-
         if (get_post_type() == 'event') {
+            $eventDate = new DateTime(get_field('event_date'));
+            $description = null;
+
+            if (has_excerpt()) {
+                $description = get_the_excerpt();
+            } else {
+                $description = wp_trim_words(get_the_content(), 18);
+            }
+
             array_push($results['events'], array(
                 'postType' => get_post_type(),
                 'title' => get_the_title(),
@@ -81,6 +83,73 @@ function universitySearchResults($data) {
         }
     }
 
+    // If programs were found, also find campuses related to those programs
+    if ($results['programs']) {
+        $programsMetaQuery = array('relation' => 'OR');
+
+        foreach($results['programs'] as $item) {
+            array_push($programsMetaQuery, array(
+                'key' => 'related_program',
+                'compare' => 'LIKE',
+                'value' => '"' . $item['id'] . '"',
+            ));
+        }
+
+        $programRelationshipQuery = new WP_Query(array(
+                'post_type' => array('professor', 'event', 'campus'),
+                'meta_query' => $programsMetaQuery,
+            )
+        );
+
+        while($programRelationshipQuery->have_posts()) {
+            $programRelationshipQuery->the_post();
+
+            if (get_post_type() == 'event') {
+                $eventDate = new DateTime(get_field('event_date'));
+                $description = null;
+
+                if (has_excerpt()) {
+                    $description = get_the_excerpt();
+                } else {
+                    $description = wp_trim_words(get_the_content(), 18);
+                }
+
+                array_push($results['events'], array(
+                    'postType' => get_post_type(),
+                    'title' => get_the_title(),
+                    'link' => get_the_permalink(),
+                    'authorName' => get_the_author(),
+                    'month' => $eventDate->format('M'),
+                    'day' => $eventDate->format('d'),
+                    'description' => $description,
+                ));
+            }
+
+            if (get_post_type() == 'professor') {
+                array_push($results['professors'], array(
+                    'postType' => get_post_type(),
+                    'title' => get_the_title(),
+                    'link' => get_the_permalink(),
+                    'authorName' => get_the_author(),
+                    'image' => get_the_post_thumbnail_url(0, 'professorLandscape'),
+                ));
+            }
+
+            if (get_post_type() == 'campus') {
+                array_push($results['campuses'], array(
+                    'postType' => get_post_type(),
+                    'title' => get_the_title(),
+                    'link' => get_the_permalink(),
+                    'authorName' => get_the_author(),
+                ));
+            }
+        }
+
+        $results['professors'] = array_values(array_unique($results['professors'], SORT_REGULAR));
+        $results['events'] = array_values(array_unique($results['events'], SORT_REGULAR));
+        $results['campuses'] = array_values(array_unique($results['campuses'], SORT_REGULAR));
+    }
+    
     wp_reset_postdata();
     return $results;
 }
